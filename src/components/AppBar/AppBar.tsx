4c241468a8a4999371eaef7f8bdcd3604397428a
zaksenards <zaksenards@gmail.com>
2024-10-03 04:43:18 -0300

    Fixed: Content view displaying previous searches

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -3,18 +3,32 @@ import { TextInput, View } from "react-native";
 
 import styles from "./styles";
 import theme from "@src/theme";
+import { useEffect, useMemo, useState } from "react";
+
+let countdown: any = -1;
+const miliseconds = 1000;
 
 type AppBarProps = {
 	onQueryChange: (query: string) => void;
 };
 
 export default function AppBar(props: AppBarProps) {
+	const [query, setQuery] = useState<string>();
+
 	return (
 		<View style={styles.conteiner}>
 			<View style={styles.labelHolder}>
 				<TextInput
 					placeholder="Search"
-					onChangeText={(text) => props.onQueryChange(text)}
+					onChangeText={(text) => {
+						setQuery(text);
+
+						clearInterval(countdown);
+						countdown = setInterval(
+							() => props.onQueryChange(text),
+							miliseconds
+						);
+					}}
 					placeholderTextColor={theme.white.hintTextColor}
 					style={styles.input}
 				/>
